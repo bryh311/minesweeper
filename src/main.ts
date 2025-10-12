3110645963a49249d4e3a0cb9c95109601bbea82
Bryce Hill <bryh311@gmail.com>
2025-10-11 22:19:41 -0400

    style refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import './style.css'
+import "./resources/style.css";
 
-import { MineSweeper } from './minesweeper.ts'
-import { init } from './minesweeper-ui.ts';
+import { MineSweeper } from "./minesweeper.ts";
+import { init } from "./minesweeper-ui.ts";
 
 const app = document.getElementById("app")!;
-const m = new MineSweeper(10, 10, 30);
-init(10, 10, app, m);
+const m = new MineSweeper(20, 20, 10);
+init(20, 20, app, m);
 
 // m.check(Math.floor(m.width / 2), Math.floor(m.width / 2));
 // m.printGrid();
